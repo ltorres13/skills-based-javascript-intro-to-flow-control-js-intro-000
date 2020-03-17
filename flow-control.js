b26f926cb5e49f89cd3e1428abecada11d38b4ff
Lauren Torres <laurentorres6554@gmail.com>
2020-03-17 03:47:41 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,18 @@
 function basicTeenager(age) {
-  if (age > 13 && age < 19) {
+if (age > 13 && age < 19) {
 return "You are a teenager!"
-}
-  else {
-    return;
-
+ }
 }
 
 function teenager(age) {
-
+  if (age < 13) {
+    return "You are not a teenager"
+  }
+  else if (age > 13 && age < 19) {
+  return "You are a teenager!"
+  }
+  else {
+    return  "You are not a teenager" }
 }
 
 function ageChecker(age) {
